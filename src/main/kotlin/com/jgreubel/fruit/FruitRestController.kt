534cbdda3b1f4ffafd201dab855938cf5ef80e1a
Joe Greubel <joegreubel1@gmail.com>
2018-09-13 22:29:07 -0700

    Add pageable to controller

--- src/main/kotlin/com/jgreubel/fruit/FruitRestController.kt
+++ src/main/kotlin/com/jgreubel/fruit/FruitRestController.kt
@@ -1,6 +1,7 @@
 package com.jgreubel.fruit
 
 import com.querydsl.core.types.Predicate
+import org.springframework.data.domain.Pageable
 import org.springframework.data.querydsl.binding.QuerydslPredicate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController
 class FruitRestController(val fruitRepository: FruitRepository) {
 
     @GetMapping("/fruit")
-    fun findAll(@QuerydslPredicate(bindings = FruitQuerydslBindingCustomizer::class) predicate: Predicate): Iterable<Fruit> {
-        return fruitRepository.findAll(predicate)
+    fun findAll(@QuerydslPredicate(bindings = FruitQuerydslBindingCustomizer::class) predicate: Predicate, pageable: Pageable): Iterable<Fruit> {
+        return fruitRepository.findAll(predicate, pageable)
     }
 
 }
